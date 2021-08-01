d0699aa0fb253e157541c3f0230623c2ae3ed763
Francesco Parolini <parolini.francesco.1@gmail.com>
2021-08-01 17:35:59 +0200

    [61-70]

--- exercises/070_comptime5.zig
+++ exercises/070_comptime5.zig
@@ -111,7 +111,7 @@ pub fn main() void {
 // perform duck typing ("if it walks like a duck and it quacks
 // like a duck, then it must be a duck") to determine if the type
 // is a "duck".
-fn isADuck(possible_duck: anytype) bool {
+fn isADuck(comptime possible_duck: anytype) bool {
     // We'll use @hasDecl() to determine if the type has
     // everything needed to be a "duck".
     //
@@ -123,8 +123,8 @@ fn isADuck(possible_duck: anytype) bool {
     // Please make sure MyType has both waddle() and quack()
     // methods:
     const MyType = @TypeOf(possible_duck);
-    const walks_like_duck = ???;
-    const quacks_like_duck = ???;
+    const walks_like_duck = @hasDecl(MyType, "waddle");
+    const quacks_like_duck = @hasDecl(MyType, "quack");
 
     const is_duck = walks_like_duck and quacks_like_duck;
 
@@ -140,6 +140,6 @@ fn isADuck(possible_duck: anytype) bool {
         // error, not a runtime panic or crash!
         possible_duck.quack();
     }
-        
+
     return is_duck;
 }
